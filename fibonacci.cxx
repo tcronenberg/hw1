a16d7456f6a367dbe34b996a678309cd1287403a
Tom Cronenberg <tom.cronenberg@hhu.de>
2015-10-28 17:48:17 +0100

    did the homework

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,24 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+unsigned int N;
+int a=0;
+int b=1;
+int temp;
+
+cout << "Geben sie eine Zahl größer 1 ein!" << endl;
+cout << "N" << endl;
+cin >> N;
+
+for(int i=2; i<=N; i++){	
+  temp = a+b;
+  a = b;
+  b = temp;
+}
+
+cout << "Result:" << b << endl;
+
+return 0;
+}
